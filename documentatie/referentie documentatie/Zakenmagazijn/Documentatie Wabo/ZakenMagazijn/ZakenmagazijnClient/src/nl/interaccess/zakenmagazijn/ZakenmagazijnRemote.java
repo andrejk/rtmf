75d3a0fec3c16ab8be18f6cb6325734acd6412e3
Andrej Koelewijn <andrej.koelewijn@it-eye.nl>
2011-04-29 15:25:05 +0200

    Rotterdam specifieke documentatie verwijderd

--- documentatie/referentie documentatie/Zakenmagazijn/Documentatie Wabo/ZakenMagazijn/ZakenmagazijnClient/src/nl/interaccess/zakenmagazijn/ZakenmagazijnRemote.java
+++
@@ -1,8 +0,0 @@
-package nl.interaccess.zakenmagazijn;
-
-import javax.ejb.Remote;
-
-@Remote
-public interface ZakenmagazijnRemote extends Zakenmagazijn {
-    public Object process(Object request) throws Exception;
-}
--- documentatie/referentie documentatie/Zakenmagazijn/zm-tar-1.1.0.0/zm-tar-1.1.0.0/07.Sources/readme.txt
+++
@@ -1,2 +0,0 @@
-With a subversion repository, browse to:
-svn://andromeda.ioo.local/projecten/zakenmagazijn/tags/
