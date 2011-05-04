cc5aba6d3d9548c15a012f82f6cd225a24a280f0
Andrej Koelewijn <andrej.koelewijn@it-eye.nl>
2011-05-04 11:53:37 +0200

    TMFPortal code verwijderd

--- tmfportaal/tmfportaal-v1.1.5-src/tmf-portaal/TMFCore-EBMS-WUSlite/src/main/java/com/ovsoftware/ictu/osb/gateway/tmfcore/ebms/wuslite/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://wus.tmf.gbo.overheid.nl/wsdl/aanmeldService-V1.1.xsd", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.ovsoftware.ictu.osb.gateway.tmfcore.ebms.wuslite;
